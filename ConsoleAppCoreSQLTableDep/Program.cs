1108f12e86fb83143072107c424b2d90b2f57deb
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-08-24 13:57:49 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppCoreSQLTableDep/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleAppCoreSQLTableDep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=LAPTOP-EA5G5R7I\SQLEXPRESS; Initial Catalog=SQLTableDependency; Integrated Security = true;";
+            using (TableDependency.SqlClient.SqlTableDependency<Modelos.Cliente> dep =
+                new TableDependency.SqlClient.SqlTableDependency<Modelos.Cliente>(connectionString))
+            {
+                dep.OnChanged += Dep_OnChanged;
+                dep.Start();
+
+                Console.WriteLine("Presione <enter> para salir");
+                Console.ReadLine();
+
+                dep.Stop();
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void Dep_OnChanged(object sender, TableDependency.SqlClient.Base.EventArgs.RecordChangedEventArgs<Modelos.Cliente> e)
+        {
+            Console.WriteLine(e.Entity.Nombres);
+        }
+    }
+}
---
+++ Modelos/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Modelos
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Documento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public DateTime Fecha_Registro { get; set; }
+        public byte Hijos { get; set; }
+        public decimal Sueldo { get; set; }
+    }
+}
